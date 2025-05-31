dae96d67914d6569884f8a364c782c12198adec1
OSwego <syava23223@gmail.com>
2025-05-31 12:42:13 +0300

    Add JSON MessageConverter to RabbitTemplate

--- src/main/java/com/example/telegramclientposter/config/RabbitMQConfig.java
+++ src/main/java/com/example/telegramclientposter/config/RabbitMQConfig.java
@@ -4,6 +4,10 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter; // Import Jackson2JsonMessageConverter
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +15,22 @@ import static com.example.telegramclientposter.constanta.Constants.*;
 
 @Configuration
 public class RabbitMQConfig {
+
+    // Define the JSON MessageConverter as a Spring Bean
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    // Override the default RabbitTemplate to use our JSON converter
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         MessageConverter jsonMessageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter);
+        return rabbitTemplate;
+    }
+
 
     // 1. Очередь для Ollama
     @Bean
