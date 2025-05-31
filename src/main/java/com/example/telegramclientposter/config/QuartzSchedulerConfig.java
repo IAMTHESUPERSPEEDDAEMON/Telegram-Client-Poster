b35d01b1b3ae1b57365c31cdd784a6b5735d3d71
OSwego <syava23223@gmail.com>
2025-05-31 13:34:16 +0300

    Add Schedule to update subscribers in db

---
+++ src/main/java/com/example/telegramclientposter/config/QuartzSchedulerConfig.java
@@ -0,0 +1,32 @@
+package com.example.telegramclientposter.config;
+
+import com.example.telegramclientposter.util.SubscribersUpdateJob;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.quartz.CronScheduleBuilder.cronSchedule;
+
+@Configuration
+public class QuartzSchedulerConfig {
+
+    @Bean
+    public JobDetail telegramSubscriberUpdateJobDetail() {
+        return JobBuilder.newJob(SubscribersUpdateJob.class)
+                .withIdentity("telegramSubscribersUpdateJob") // Имя задачи
+                .storeDurably() // Сохранять, даже если нет активных триггеров
+                .build();
+    }
+
+    @Bean
+    public Trigger telegramSubscriberUpdateTrigger() {
+        return TriggerBuilder.newTrigger()
+                .forJob(telegramSubscriberUpdateJobDetail())
+                .withIdentity("telegramSubscribersUpdateJob")
+                .withSchedule(cronSchedule("0 23 * * *"))
+                .build();
+    }
+}
